b6a08e40990d0a9110b4eda177b9fc0b10b1030f
Altovisk <ericaltao13@gmail.com>
2023-09-29 19:23:39 -0300

    modificação objeto Jogador

--- projeto 2/pratica/Jogador.js
+++ projeto 2/pratica/Jogador.js
@@ -1,5 +1,5 @@
-class Jogador{
-    constructor(ctx, canvas, teclado){
+class Jogador {
+    constructor(ctx, canvas, teclado) {
         this.ctx = ctx;
         this.canvas = canvas;
         this.teclado = teclado;
@@ -19,76 +19,112 @@ class Jogador{
         this.altImg = this.jogador.height;
         this.largSprite = this.largImg / this.SpriteX;
         this.altSprite = this.altImg / this.SpriteY;
-        
-        this.jogador.addEventListener('load', ()=>{
+
+        this.jogador.addEventListener('load', () => {
             this.desenhar()
         })
     }
 
-    gerenciar(){
-        if(this.teclado.direita){
+    gerenciar() {
+        if (this.teclado.direita) {
             this.px += this.velocidade
-        }else if(this.teclado.esquerda){
+        } else if (this.teclado.esquerda) {
             this.px -= this.velocidade
-        }else if(this.teclado.cima){
+        } else if (this.teclado.cima) {
             this.py -= this.velocidade
-        }else if(this.teclado.baixo){
+        } else if (this.teclado.baixo) {
             this.py += this.velocidade
         }
     }
 
-    animacao(){
-            let cWidth = this.canvas.width;
-            let cHeigth = this.canvas.height;
-            
-                if (this.teclado.direita) {
-                    this.px += this.velocidade;
-                    this.numSprite++
-                    if (this.numSprite >= 5) {
-                        this.numSprite = 0;
-                    }
-                    if(this.px + this.largSprite >= cWidth - 36){
-                        this.px = cWidth - this.largSprite - 36
-                    }
-                } else if (this.teclado.esquerda) {
-                    this.px -= this.velocidade;
-                    this.numSprite++
-                    if (this.numSprite >= 5) {
-                        this.numSprite = 0;
-                    }
-                     if (this.px <= -54) {
-                         this.px = -54;
-                     }
-                }
-                if (this.teclado.cima) {
-                    this.py -= this.velocidade;
+    animacao() {
+        let cWidth = this.canvas.width;
+        let cHeigth = this.canvas.height;
 
-                    this.numSprite++
-                    if (this.numSprite >= 5) {
-                        this.numSprite = 0;
-                    }
-                    if (this.py <= -34) {
-                         this.py = -34;
-                     }
-                } else if (this.teclado.baixo) {
-                    this.py += this.velocidade;
-                    this.numSprite++
-                    if (this.numSprite >= 5) {
-                        this.numSprite = 0;
-                    }
-                    if (this.py + this.altSprite >= cHeigth - 36) {
-                        this.py = cHeigth - this.altSprite - 36;
-                     }
-                }
-
-                setTimeout(() => {
-                    requestAnimationFrame(this.animacao);
-                }, 50);
+        if (this.teclado.direita) {
+            this.px += this.velocidade;
+            this.numSprite++
+            if (this.numSprite >= 5) {
+                this.numSprite = 0;
             }
+            if (this.px + this.largSprite >= cWidth - 36) {
+                this.px = cWidth - this.largSprite - 36
+            }
+        } else if (this.teclado.esquerda) {
+            this.px -= this.velocidade;
+            this.numSprite++
+            if (this.numSprite >= 5) {
+                this.numSprite = 0;
+            }
+            if (this.px <= -54) {
+                this.px = -54;
+            }
+        }
+        if (this.teclado.cima) {
+            this.py -= this.velocidade;
 
-    desenhar(){
+            this.numSprite++
+            if (this.numSprite >= 5) {
+                this.numSprite = 0;
+            }
+            if (this.py <= -34) {
+                this.py = -34;
+            }
+        } else if (this.teclado.baixo) {
+            this.py += this.velocidade;
+            this.numSprite++
+            if (this.numSprite >= 5) {
+                this.numSprite = 0;
+            }
+            if (this.py + this.altSprite >= cHeigth - 36) {
+                this.py = cHeigth - this.altSprite - 36;
+            }
+        }
+
+        setTimeout(() => {
+            requestAnimationFrame(this.animacao);
+        }, 50);
+    }
+
+    sprites() {
+        window.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowRight') {
+                direita = true;
+                colunSprite = 6;
+
+            } else if (event.key === 'ArrowLeft') {
+                esquerda = true;
+                colunSprite = 7;
+            }
+            if (event.key === 'ArrowUp') {
+                cima = true;
+                colunSprite = 5;
+            } else if (event.key === 'ArrowDown') {
+                baixo = true;
+                colunSprite = 4;
+            }
+        })
+
+        window.addEventListener('keyup', (event) => {
+            if (event.key === 'ArrowRight') {
+                direita = false;
+            } else if (event.key === 'ArrowLeft') {
+                esquerda = false;
+            }
+            if (event.key === 'ArrowUp') {
+                cima = false;
+            } else if (event.key === 'ArrowDown') {
+                baixo = false;
+            }
+            if (!direita && !esquerda && !cima && !baixo) {
+                colunSprite = 0;
+                numSprite = 0;
+            }
+        })
+    }
+    desenhar() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        
+
         this.gerenciar();
         this.animacao();
 
